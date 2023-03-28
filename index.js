1b4052d2156ec550e981eb84964a1098af5fd866
SlimNoob <slimakovskiy@gmail.com>
2023-03-28 22:41:36 +0300

    startup1

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 "use strict";
 
-console.log(1990 + +'5');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
 
-incr++;
-decr--;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(incr, decr);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
